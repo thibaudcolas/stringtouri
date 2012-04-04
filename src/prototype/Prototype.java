74e5e80c9b8e5f9dc53afe10e0a7f1fc5dc18e17
ThibWeb <ThibWeb@a13b38d8-f8a5-4b66-8729-c97cf40cb9bf>
2012-04-04 00:44:21 +0000

    Passage à l'API 2.6.4

--- src/prototype/Prototype.java
+++ src/prototype/Prototype.java
@@ -12,10 +12,10 @@ public class Prototype {
 	public static void main(String[] args) {
 		App test = null;
 		try {
-			test = new AppRDF("./rdf/insee/","./rdf/passim-propre.rdf","departements","");
-			test.setLiaisonTypee("geo:nom", "passim:department", "geo:Departement", "");
-			test.setRDFOutput();
-			test.initiateInterconnexion(true);
+			test = new AppSesame("http://localhost:8080/openrdf-sesame/","geo-insee","passim-test");
+			test.setLiaisonTypee("geo:nom", "passim:centerTown", "geo:Commune", "");
+			test.setSPARQLOutput();
+			test.initiateInterconnexion(false);
 			System.out.println(test.getOutput());
 		} 
 		catch (Exception e) {
